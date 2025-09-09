952ff3379306a393daa4a2c6b6ae1faff579042e
Khant Kyaw <khantkyaw.contact@gmail.com>
2025-09-09 15:58:12 +0630

    add profilepopup

---
+++ profilepopup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profilepopup</title>
+    <link rel="stylesheet" href="profilepopup.css">
+</head>
+<body>
+    <section id="profile-dropdown">
+  <div class="profile-menu">
+    <div class="user-info">
+      <img src="/page/34a4bf82-ee08-4320-9587-0db497d8b272/images/88b69fe2a5b5d59fc2eff4ebb93b78b49d8f35f6.png" alt="User avatar" class="avatar">
+      <div class="user-details">
+        <span class="user-name">Kenny</span>
+        <span class="user-tag">(you)</span>
+      </div>
+    </div>
+    <nav class="menu-nav">
+      <ul>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Settings</a></li>
+        <li><a href="#" class="logout-link">Logout</a></li>
+      </ul>
+    </nav>
+  </div>
+</section>
+</body>
+</html>
