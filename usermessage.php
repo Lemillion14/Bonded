fcba0be1c1315f3a8e2243cf2faad86595bdc258
K3nny03 <khantkyaw.contact@gmail.com>
2025-09-10 15:00:55 +0630

    hi

---
+++ usermessage.php
@@ -0,0 +1,43 @@
+<?php include 'userheader.php' ?>
+
+<section id="section-chat">
+    <div class="page-container">
+        <main class="chat-container">
+            <aside class="matches-sidebar">
+                <h2 class="sidebar-title">New Matches</h2>
+                <ul class="matches-list">
+
+                    <li class="match-item">
+                        <img class="avatar" src="./images/account.png" alt="Shoon avatar">
+                        <span class="match-name">Shoon</span>
+                        <span class="match-time">Yesterday</span>
+                    </li>
+                    <li class="match-item">
+                        <img class="avatar" src="./images/account-2.png" alt="Linn Latt avatar">
+                        <span class="match-name">Linn Latt</span>
+                        <span class="match-time">Monday</span>
+                    </li>
+                    <li class="match-item">
+                        <img class="avatar" src="./images/account-3.png" alt="Akari Nyein avatar">
+                        <span class="match-name">Akari Nyein</span>
+                        <span class="match-time">25/8/2025</span>
+                    </li>
+                </ul>
+            </aside>
+            <div class="chat-panel">
+                <div class="message-area">
+                    <!-- Chat messages would appear here -->
+                </div>
+                <form class="message-form">
+                    <div class="input-container">
+                        <input type="text" class="message-input" placeholder="Send a message...">
+                    </div>
+                    <button type="button" class="send-button">
+                        <img src="./images/id-card.png" alt="Share info icon">
+                    </button>
+                </form>
+            </div>
+        </main>
+    
+    </div>
+</section>
