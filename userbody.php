2e7012f606c8ac431b5062cfb2169f3df2623d99
K3nny03 <khantkyaw.contact@gmail.com>
2025-09-10 15:03:11 +0630

    hi

---
+++ userbody.php
@@ -0,0 +1,30 @@
+<?php include 'userheader.php'; ?>
+
+<main id="section-main" class="main-content">
+  <div class="profile-card-wrapper">
+    <div class="profile-card glass-effect">
+      <div class="action-buttons">
+        <!-- merged image -->
+        <button class="action-button">
+          <img class="button-bg" src="./images/circle.png" alt="Back button background">
+          <img class="button-icon" src="./images/back.png" alt="Back icon">
+        </button>
+        <!-- merged image -->
+        <button class="action-button">
+          <img class="button-bg" src="./images/circle.png" alt="Pass button background">
+          <img class="button-icon" src="./images/cross.png" alt="Pass icon">
+        </button>
+        <!-- merged image -->
+        <button class="action-button">
+          <img class="button-bg" src="./images/circle.png" alt="Like button background">
+          <img class="button-icon" src="./images/love-3.png" alt="Like icon" style="width: 25px; height: 25px;">
+        </button>
+        <!-- merged image -->
+        <button class="action-button">
+          <img class="button-bg" src="./images/circle.png" alt="Favourite button background">
+          <img class="button-icon" src="./images/favourite.png" alt="Favourite icon">
+        </button>
+      </div>
+    </div>
+  </div>
+</main>
